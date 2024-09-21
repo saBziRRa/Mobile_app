5276cc0b606aad9aaa362902eed786e827f8a9f6
tsemenko <semenko.tima@yandex.ru>
2024-09-21 17:08:02 +0300

    feat: add form

--- Readme.md
+++ Readme.md
@@ -8,3 +8,5 @@ ionic serve
 ```
 * Методы: интерпаляция переменных {{}}(двойные фигурные)
 * Открыть предпросмотр markdown ctrl + k затем отпустить и нажать v
+* https://test-bc740.web.app/developing/starting
+* Копирование ctrl + insert
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -10,7 +10,12 @@ export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
-
+  val: string = "----"
+  val1:string = "?"
+  val2:string = "?"
+  btnClick(){
+this.val=this.val1
+  }
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
