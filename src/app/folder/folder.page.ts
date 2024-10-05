6ca5674bdf26586edf4f759546c03ba36600c904
Your Name <you@example.com>
2024-10-05 17:00:40 +0300

    fit Добавление таблицы

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,6 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+interface GameType {
+  Game_name: string
+  Game_autor: string
+}
+
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
@@ -10,6 +15,11 @@ export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
+  list_name: string = "My_Games"
+  input_Name: string = "--"
+  input_Autor: string = "--"
+  set_value: string = "__"
+  games: GameType[] = []
   val: string = "----"
   val1:string = "?"
   val2:string = "?"
@@ -17,7 +27,8 @@ export class FolderPage implements OnInit {
 this.val=this.val1
   }
   btnClick2(){
-this.val1=this.val2
+ this.games.push({Game_name: this.input_Name, Game_autor:this.input_Autor})
+ this.set_value = this.input_Name
   }
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
